65b65da101824d85eefc976da26f31522695a48c
Diego Vilhalva <diegorocha450@yahoo.com>
2024-08-08 12:22:34 -0300

    sections added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,22 @@
 
 const preloader = document.querySelector("[data-preaload]")
 
-window.addEventListener("load",function () {
+window.addEventListener("load", function () {
     preloader.classList.add("loaded")
     document.body.classList.add('loaded')
 })
 
 
-const  addEventOnElements = function (elements,eventType,callback) {
-    for (let i = 0,len = elements.length; i < len; i++) {
-        elements[i].addEventListener(eventType,callback)
-        
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback)
+
     }
 
 }
 
 
-const navbar  = document.querySelector("[data-navbar]")
+const navbar = document.querySelector("[data-navbar]")
 const navTogglers = document.querySelectorAll("[data-nav-toggler]")
 const overlay = document.querySelector("[data-overlay]")
 
@@ -28,14 +28,14 @@ const toggleNavbar = function () {
     document.body.classList.toggle("nav-active")
 }
 
-addEventOnElements(navTogglers,"click",toggleNavbar)
+addEventOnElements(navTogglers, "click", toggleNavbar)
 
 const header = document.querySelector("[data-header]")
 
 let lastScrollPos = 0
 
 const hideHeader = function () {
-    const isScrollBottom =  lastScrollPos < window.scrollY
+    const isScrollBottom = lastScrollPos < window.scrollY
 
     if (isScrollBottom) {
         header.classList.add("hide")
@@ -47,11 +47,88 @@ const hideHeader = function () {
 }
 
 
-window.addEventListener("scroll",function () {
-    if (window.scrollY >= 50 ) {
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 50) {
         header.classList.add("active")
         hideHeader()
-    }else{
+    } else {
         header.classList.remove("active")
+    }
+})
+
+
+const heroSlider = document.querySelector("[data-hero-slider]")
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]")
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]")
+const heroSliderNextBtn = document.querySelector("[data-next-btn]")
+
+let currentSlidePos = 0
+
+let lastActiveSliderItem = heroSliderItems[0]
+
+
+const updateSliderPos = function () {
+    lastActiveSliderItem.classList.remove("active")
+    heroSliderItems[currentSlidePos].classList.add("active")
+    lastActiveSliderItem = heroSliderItems[currentSlidePos]
+}
+
+const slideNext = function () {
+    if (currentSlidePos >= heroSliderItems.length - 1) {
+        currentSlidePos = 0
+    } else {
+        currentSlidePos++
+    }
+    updateSliderPos()
+}
+
+heroSliderNextBtn.addEventListener("click", slideNext)
+
+
+const slidePrev = function () {
+    if (currentSlidePos <= 0) {
+        currentSlidePos = heroSliderItems.length - 1
+    } else {
+        currentSlidePos--;
+    }
+    updateSliderPos()
+}
+
+heroSliderPrevBtn.addEventListener("click", slidePrev)
+
+let autoSlideInterval
+
+
+const autoSlide = function () {
+    autoSlideInterval = setInterval(function () {
+        slideNext()
+    }, 7000)
+}
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseover", function () {
+    clearInterval(autoSlideInterval)
+})
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], "mouseout".autoSlide);
+
+window.addEventListener("load", autoSlide);
+
+
+const parallaxItems = document.querySelectorAll("[data-parallax-item]")
+
+let x, y;
+
+window.addEventListener("mousemove", function (event) {
+    x = (event.clientX / window.innerWidth * 10) - 5;
+    y = (event.clientY / window.innerHeight * 10) - 5;
+
+    x = x - (x * 2)
+    y = y - (y * 2)
+
+    for (let i = 0, len = parallaxItems.length; i < len; i++ ){
+        x = x * Number(parallaxItems[i].dataset.parallaxSpeed)
+        y = y * Number(parallaxItems[i].dataset.parallaxSpeed)
+
+        parallaxItems[i].style.transform = `translate3d(${x}px,${y}px,0px)`
     }
 })
